d083c60e08b5d22cf854c321a35dfc298bbd10e9
Dmitry <realcomp@Mac-mini.local>
2024-12-18 02:28:34 +0200

    понятные сообщения о принтере

--- server.js
+++ server.js
@@ -51,23 +51,26 @@ app.post('/api/print', (req, res) => {
 });
 
 // Маршрут для отправки ZPL на принтер
+
+
 app.post('/send-to-printer', (req, res) => {
     const { zpl, printer_host, printer_port } = req.body;
 
     const PRINTER_HOST = printer_host || '192.168.0.100'; // Значение по умолчанию
-    const PRINTER_PORT = printer_port || 9100; // Значение по умолчанию
+    const PRINTER_PORT = printer_port || 9100;
 
     const client = new net.Socket();
+
     client.connect(PRINTER_PORT, PRINTER_HOST, () => {
-        console.log(`Соединение с принтером ${PRINTER_HOST}:${PRINTER_PORT} установлено`);
+        console.log(`Connected to printer at ${PRINTER_HOST}:${PRINTER_PORT}`);
         client.write(zpl);
         client.end();
-        res.status(200).send('Данные успешно отправлены на принтер');
+        res.status(200).send('ZPL successfully sent to the printer');
     });
 
     client.on('error', (err) => {
-        console.error('Ошибка при отправке на принтер:', err.message);
-        res.status(500).send(`Ошибка отправки на принтер: ${err.message}`);
+        console.error('Printer error:', err.message);
+        res.status(500).send(`Error: Unable to connect to printer at ${PRINTER_HOST}:${PRINTER_PORT}. Please check the printer configuration.`);
     });
 });
 
